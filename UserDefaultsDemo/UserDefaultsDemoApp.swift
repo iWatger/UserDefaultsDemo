fd2bbb25cdfce5f81cd10e5b71e9a995ac22417c
iWatger <ethanfox646@icloud.com>
2023-02-27 15:26:28 -0500

    Initial Commit

---
+++ UserDefaultsDemo/UserDefaultsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsDemoApp.swift
+//  UserDefaultsDemo
+//
+//  Created by Ethan Fox on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
